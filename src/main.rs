4bde5b4fe1c2bd80db6d0d5f2a58d1f4bd3c2db6
Chris West (Faux) <git@goeswhere.com>
2021-05-23 16:20:33 +0100

    chore: might do dns lookups but no way to test /o/

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
-use std::collections::HashSet;
-use std::convert::TryFrom;
-use std::net::{IpAddr, SocketAddr};
-use std::str::FromStr;
-
 use anyhow::anyhow;
+use anyhow::bail;
 use anyhow::ensure;
+use anyhow::Context;
 use anyhow::Result;
-use anyhow::{bail, Context};
-use k8s_openapi::api::core::v1::{Endpoints, Pod};
-use kube::{Api, Client};
-use lazy_static::lazy_static;
 use log::error;
 use log::info;
-use regex::Regex;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
-use trust_dns_resolver::system_conf::read_system_conf;
-use trust_dns_resolver::{Name, Resolver};
 
 use crate::k8s::find_dns;
+use crate::resolve::ResolveCtx;
 
 mod k8s;
+mod resolve;
 
 #[derive(Debug)]
 enum ConnectType<'b> {
@@ -68,95 +59,12 @@ async fn read_initialisation<'b>(
     }
 }
 
-// resolution order:
-// $0.default.svc.local.
-// # $0.svc.local.
-// $0.default.pod.local.
-// # $0.pod.local.
-// $0.default.pod-by-name.local.
-// # $0.pod-by-name.local.
-
-// then: cluster dns with:
-// $0.default.svc.cluster.local
-// $0.svc.cluster.local
-// $0.cluster.local
-async fn resolve(client: &Client, hostname: &str) -> Result<Vec<SocketAddr>> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(concat!(
-            "^([a-zA-Z0-9-]{1,63})(?:\\.([a-zA-Z0-9-]{1,63}))?",
-            "\\.(svc|pod|pod-by-name)\\.local\\.?(?::(\\d+))?$"
-        ))
-        .unwrap();
-    }
-
-    if let Some(custom) = RE.captures(hostname) {
-        let name: &str = &custom[1];
-        let ns: &str = &custom.get(2).map(|v| v.as_str()).unwrap_or("default");
-        let command: &str = &custom[3];
-        let specified_port: Option<&str> = custom.get(4).map(|v| v.as_str());
-
-        match command {
-            "svc" => {
-                let mut ips = Vec::with_capacity(16);
-                let mut ports = HashSet::<i32>::with_capacity(4);
-                let endpoints = Api::<Endpoints>::namespaced(client.clone(), ns)
-                    .get(name)
-                    .await?;
-
-                // lazily ignoring the idea that there might be differences between sets
-                for subset in endpoints.subsets.unwrap_or_default() {
-                    for port in subset.ports.unwrap_or_default() {
-                        ports.insert(port.port);
-                    }
-
-                    for address in subset.addresses.unwrap_or_default() {
-                        ips.push(
-                            IpAddr::from_str(&address.ip)
-                                .with_context(|| anyhow!("parsing {:?}", address.ip))?,
-                        );
-                    }
-                }
-
-                if ports.is_empty() || ips.is_empty() {
-                    return Ok(Vec::new());
-                }
-
-                let port = if ports.len() == 1 {
-                    u16::try_from(
-                        ports
-                            .into_iter()
-                            .next()
-                            .expect("is of length one so has an element"),
-                    )?
-                } else if let Some(specified_port) = specified_port {
-                    // TODO: map using actual service
-                    u16::from_str(specified_port)?
-                } else {
-                    unimplemented!()
-                };
-
-                return Ok(ips
-                    .into_iter()
-                    .map(|ip| SocketAddr::new(ip, port))
-                    .collect());
-            }
-            _ => unimplemented!("{:?}", command),
-        }
-    }
-
-    return resolve_against_kube_dns(hostname).await;
-}
-
-async fn resolve_against_kube_dns(hostname: &str) -> Result<Vec<SocketAddr>> {
-    unimplemented!()
-}
-
-async fn worker(client: Client, mut source: TcpStream) -> Result<()> {
+async fn worker(resolve_ctx: ResolveCtx, mut source: TcpStream) -> Result<()> {
     let mut buf = [0; 4096];
     let init = read_initialisation(&mut source, &mut buf).await?;
     let dest = match init {
         ConnectType::Http { hostname } => {
-            let addrs = resolve(&client, &hostname).await?;
+            let addrs = resolve::resolve(resolve_ctx, &hostname).await?;
             info!("establishing connection to {:?} via {:?}", hostname, addrs);
             let conn = TcpStream::connect(&*addrs).await?;
             source.write_all(b"HTTP/1.0 200 OK\r\n\r\n").await?;
@@ -197,10 +105,9 @@ pub async fn main() -> Result<()> {
         .with_context(|| anyhow!("finding dns servers"))?;
     info!("found kube-dns: {:?}", dns);
 
-    let namespace = "default";
-
-    let pods = Api::<Pod>::namespaced(client.clone(), &namespace);
+    #[cfg(never)]
     let pod = pods.get_status("svc-flags-78966c8947-45gds").await?;
+    #[cfg(never)]
     println!(
         "{:?}",
         pod.status
@@ -217,9 +124,14 @@ pub async fn main() -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     loop {
         let (socket, client_addr) = listener.accept().await?;
-        let client = client.clone();
+        let resolve_ctx = ResolveCtx {
+            cluster_local: "cluster.local".to_string(),
+            client: client.clone(),
+            default_namespace: "default".to_string(),
+            dns_servers: dns.clone(),
+        };
         tokio::spawn(async move {
-            if let Err(e) = worker(client, socket).await {
+            if let Err(e) = worker(resolve_ctx, socket).await {
                 error!("{:?} handling {:?}", e, client_addr);
             }
         });
---
+++ src/resolve.rs
@@ -0,0 +1,149 @@
+use std::collections::HashSet;
+use std::convert::TryFrom;
+use std::net::{IpAddr, SocketAddr};
+use std::str::FromStr;
+
+use anyhow::anyhow;
+use anyhow::Context;
+use anyhow::Result;
+use k8s_openapi::api::core::v1::Endpoints;
+use kube::Api;
+use lazy_static::lazy_static;
+use regex::Regex;
+use trust_dns_resolver::config::{NameServerConfig, Protocol, ResolverConfig, ResolverOpts};
+use trust_dns_resolver::Name;
+
+#[derive(Clone)]
+pub struct ResolveCtx {
+    pub cluster_local: String,
+    pub default_namespace: String,
+    pub client: kube::Client,
+    pub dns_servers: Vec<IpAddr>,
+}
+
+// resolution order:
+// $0.$default.endpoints.local.
+// $0.endpoints.local.
+// # $0.$default.pod.local.
+// # $0.pod.local.
+// # $0.$default.pod-by-name.local.
+// # $0.pod-by-name.local.
+
+// then: cluster dns with:
+// $0.$default.svc.cluster.local
+// $0.svc.cluster.local
+// $0.cluster.local
+pub(crate) async fn resolve(ctx: ResolveCtx, hostname: &str) -> Result<Vec<SocketAddr>> {
+    lazy_static! {
+        static ref PORT: Regex = Regex::new(concat!("^(.*)(?::(\\d+))?$")).unwrap();
+    }
+
+    let (hostname, specified_port) = {
+        match PORT.captures(hostname) {
+            Some(cap) => (cap[1].to_string(), cap.get(2).map(|m| m.as_str())),
+            None => (hostname.to_string(), None),
+        }
+    };
+
+    lazy_static! {
+        static ref RE: Regex = Regex::new(concat!(
+            "^([a-zA-Z0-9-]{1,63})(?:\\.([a-zA-Z0-9-]{1,63}))?",
+            "\\.(endpoints|pod|pod-by-name)\\.local\\.?$"
+        ))
+        .unwrap();
+    }
+
+    if let Some(custom) = RE.captures(&hostname) {
+        let name: &str = &custom[1];
+        let ns: String = custom
+            .get(2)
+            .map(|v| v.as_str().to_string())
+            .unwrap_or(ctx.default_namespace);
+        let command: &str = &custom[3];
+
+        match command {
+            "endpoints" => {
+                let mut ips = Vec::with_capacity(16);
+                let mut ports = HashSet::<i32>::with_capacity(4);
+                let endpoints = Api::<Endpoints>::namespaced(ctx.client.clone(), &ns)
+                    .get(name)
+                    .await?;
+
+                // lazily ignoring the idea that there might be differences between sets
+                for subset in endpoints.subsets.unwrap_or_default() {
+                    for port in subset.ports.unwrap_or_default() {
+                        ports.insert(port.port);
+                    }
+
+                    for address in subset.addresses.unwrap_or_default() {
+                        ips.push(
+                            IpAddr::from_str(&address.ip)
+                                .with_context(|| anyhow!("parsing {:?}", address.ip))?,
+                        );
+                    }
+                }
+
+                if ports.is_empty() || ips.is_empty() {
+                    return Ok(Vec::new());
+                }
+
+                let port = if ports.len() == 1 {
+                    u16::try_from(
+                        ports
+                            .into_iter()
+                            .next()
+                            .expect("is of length one so has an element"),
+                    )?
+                } else if let Some(specified_port) = specified_port {
+                    // TODO: map using actual service
+                    u16::from_str(specified_port)?
+                } else {
+                    unimplemented!()
+                };
+
+                return Ok(ips
+                    .into_iter()
+                    .map(|ip| SocketAddr::new(ip, port))
+                    .collect());
+            }
+            _ => unimplemented!("{:?}", command),
+        }
+    }
+
+    let port = u16::from_str(specified_port.ok_or(anyhow!(
+        "port required when not resolving endpoints, not {:?}",
+        hostname
+    ))?)?;
+
+    Ok(resolve_against_kube_dns(ctx, &hostname)
+        .await?
+        .into_iter()
+        .map(|ip| SocketAddr::new(ip, port))
+        .collect())
+}
+
+async fn resolve_against_kube_dns(ctx: ResolveCtx, hostname: &str) -> Result<Vec<IpAddr>> {
+    let mut config = ResolverConfig::new();
+    for ip in ctx.dns_servers {
+        config.add_name_server(NameServerConfig {
+            protocol: Protocol::Udp,
+            socket_addr: SocketAddr::new(ip, 53),
+            tls_dns_name: None,
+            trust_nx_responses: true,
+        });
+        config.add_search(Name::from_str(&format!(
+            "{}.svc.{}",
+            &ctx.default_namespace, &ctx.cluster_local
+        ))?);
+        config.add_search(Name::from_str(&format!("svc.{}", &ctx.cluster_local))?);
+        config.add_search(Name::from_str(&ctx.cluster_local)?);
+    }
+    Ok(
+        trust_dns_resolver::TokioAsyncResolver::tokio(config, ResolverOpts::default())?
+            .lookup_ip(hostname)
+            .await?
+            .into_iter()
+            .map(|v| v)
+            .collect(),
+    )
+}
